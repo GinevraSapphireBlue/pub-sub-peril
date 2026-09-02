e622a3c513934b8a42c72cf7fd1910c27a996763
GinevraSapphireBlue <anna.ol3ksiak@gmail.com>
2026-09-02 21:32:37 +0200

    Connect server to RabbitMQ and publish initial message.

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,25 @@
+import amqp from "amqplib";
+
+import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+import type { PlayingState } from "../internal/gamelogic/gamestate.js";
+import { publishJSON } from "../internal/pubsub/publish.js";
+
 async function main() {
   console.log("Starting Peril server...");
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+  const rabbitConn = await amqp.connect(rabbitConnString);
+  console.log("Connected to RabbitMQ");
+
+  const confirmChannel = await rabbitConn.createConfirmChannel();
+  const state: PlayingState = { isPaused: true };
+  await publishJSON(confirmChannel, ExchangePerilDirect, PauseKey, state);
+  console.log("Published to confirm channel");
+
+  process.on("SIGINT", async () => {
+    console.log("Shutting Peril server down");
+    await rabbitConn.close();
+    process.exit(0)
+  });
 }
 
 main().catch((err) => {
