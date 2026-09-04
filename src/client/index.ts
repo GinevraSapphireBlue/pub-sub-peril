ef152ad3ad75e34a4e2f6479796bba204a94cdeb
GinevraSapphireBlue <anna.ol3ksiak@gmail.com>
2026-09-04 23:02:18 +0200

    Client subscribes to moves of other players and publishes its own moves.

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,13 +1,14 @@
-import amqp from "amqplib";
+import amqp, { type Channel, type ConfirmChannel } from "amqplib";
 import { clientWelcome, commandStatus, getInput, printClientHelp, printQuit } from "../internal/gamelogic/gamelogic.js";
 import { declareAndBind } from "../internal/pubsub/consume.js";
-import { ExchangePerilDirect, ExchangePerilTopic, PauseKey } from "../internal/routing/routing.js";
+import { ArmyMovesPrefix, ExchangePerilDirect, ExchangePerilTopic, PauseKey } from "../internal/routing/routing.js";
 import { SimpleQueueType } from "../internal/pubsub/consume.js";
 import { GameState } from "../internal/gamelogic/gamestate.js";
 import { commandSpawn } from "../internal/gamelogic/spawn.js";
 import { commandMove } from "../internal/gamelogic/move.js";
 import { subscribeJSON } from "../internal/pubsub/subscribe.js";
-import { handlerPause } from "./handlers.js";
+import { handlerMove, handlerPause } from "./handlers.js";
+import { publishJSON } from "../internal/pubsub/publish.js";
 
 async function main() {
   console.log("Starting Peril client...");
@@ -16,14 +17,23 @@ async function main() {
   
   const username = await clientWelcome();
 
+  // Confirm channel
+  const confirmChannel = await rabbitConn.createConfirmChannel();
+
+  // Connect to channel and queue with pause/resume messages
   const [channel, queue] = await declareAndBind(rabbitConn, ExchangePerilDirect, `${PauseKey}.${username}`, PauseKey, SimpleQueueType.Transient);
 
   const gameState = new GameState(username);
 
+  // Subscribe to pause/resume messages
   await subscribeJSON(rabbitConn, ExchangePerilDirect, `${PauseKey}.${username}`, PauseKey, SimpleQueueType.Transient, handlerPause(gameState));
 
-  await processCommands(gameState);
-  
+  // Subscribe to moves of other players
+  await subscribeJSON(rabbitConn, ExchangePerilTopic, `${ArmyMovesPrefix}.${username}`, `${ArmyMovesPrefix}.*`, SimpleQueueType.Transient, handlerMove(gameState));
+
+  // Command loop
+  await processCommands(gameState, confirmChannel, username);
+
   process.exit(0);
 }
 
@@ -32,7 +42,7 @@ main().catch((err) => {
   process.exit(1);
 });
 
-async function processCommands(gameState: GameState): Promise<void> {
+async function processCommands(gameState: GameState, confirmChannel: ConfirmChannel, username: string): Promise<void> {
   while (true) {
     const words = await getInput();
     if (words.length === 0)
@@ -48,7 +58,9 @@ async function processCommands(gameState: GameState): Promise<void> {
     }
     else if (command === "move") {
       try {
-        commandMove(gameState, words);
+        const move = commandMove(gameState, words);
+        await publishJSON(confirmChannel, ExchangePerilTopic, `${ArmyMovesPrefix}.${username}`, move);
+        console.log(`Move ${words.slice(1)} was published`);
       } catch (err) {
         console.log(err);
       }
