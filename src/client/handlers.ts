17fb7507762ea2b9096c8b19bcb57782cc3b3abb
GinevraSapphireBlue <anna.ol3ksiak@gmail.com>
2026-09-06 22:20:59 +0200

    Add Ack, Nack handling to client message handlers.

--- src/client/handlers.ts
+++ src/client/handlers.ts
@@ -1,18 +1,26 @@
 import type { ArmyMove } from "../internal/gamelogic/gamedata.js";
 import { GameState, type PlayingState } from "../internal/gamelogic/gamestate.js";
-import { handleMove } from "../internal/gamelogic/move.js";
+import { handleMove, MoveOutcome } from "../internal/gamelogic/move.js";
 import { handlePause } from "../internal/gamelogic/pause.js";
+import type { AckType } from "../internal/pubsub/subscribe.js";
 
-export function handlerPause(gs: GameState): (ps: PlayingState) => void {
+export function handlerPause(gs: GameState): (ps: PlayingState) => AckType {
   return (ps) => {
     handlePause(gs, ps);
     console.log("> ");
+    return "Ack";
   };
 }
 
-export function handlerMove(gs: GameState): (move: ArmyMove) => void {
+export function handlerMove(gs: GameState): (move: ArmyMove) => AckType {
   return (move) => {
-    handleMove(gs, move);
+    const moveOutcome = handleMove(gs, move);
     console.log("> ");
+    if (moveOutcome === MoveOutcome.Safe || moveOutcome === MoveOutcome.MakeWar) {
+      return "Ack";
+    }
+    else {
+      return "NackDiscard";
+    }
   };
 }
--- src/internal/pubsub/subscribe.ts
+++ src/internal/pubsub/subscribe.ts
@@ -1,20 +1,31 @@
 import amqp from "amqplib";
 import { declareAndBind, SimpleQueueType } from "./consume.js";
 
+export type AckType = "Ack" | "NackRequeue" | "NackDiscard";
+
 export async function subscribeJSON<T>(
   conn: amqp.ChannelModel,
   exchange: string,
   queueName: string,
   key: string,
   queueType: SimpleQueueType,
-  handler: (data: T) => void,
+  handler: (data: T) => AckType,
 ): Promise<void> {
   const [channel, queue] = await declareAndBind(conn, exchange, queueName, key, queueType);
   const msgConsumed = await channel.consume(queueName, (msg: amqp.ConsumeMessage | null) => {
     if (!msg)
       return;
     const parsedMsg = JSON.parse(msg.content.toString());
-    handler(parsedMsg);
-    channel.ack(msg);
+    const acktype = handler(parsedMsg);
+    if (acktype === "Ack") {
+      console.log("subscribeJSON(): Recived Ack");
+      channel.ack(msg);
+    } else if (acktype === "NackRequeue") {
+      console.log("subscribeJSON(): Received NackRequeue");
+      channel.nack(msg, false, true);
+    } else if (acktype === "NackDiscard") {
+      console.log("subscribeJSON(): Received NackDiscard");
+      channel.nack(msg, false, false);
+    }
   });
 }
