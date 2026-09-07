3123dcefac090dff008f4a595f0ce204c74de9fa
GinevraSapphireBlue <anna.ol3ksiak@gmail.com>
2026-09-07 22:49:55 +0200

    Add MsgPack as an alternative serialization format for publishing messages

--- src/internal/pubsub/publish.ts
+++ src/internal/pubsub/publish.ts
@@ -1,3 +1,4 @@
+import { encode } from "@msgpack/msgpack";
 import type { ConfirmChannel } from "amqplib";
 
 export async function publishJSON<T>(
@@ -8,4 +9,15 @@ export async function publishJSON<T>(
 ): Promise<void> {
   const bufferedValue = Buffer.from(JSON.stringify(value));
   ch.publish(exchange, routingKey, bufferedValue, { contentType: "application/json" });
+}
+
+export async function publishMsgPack<T>(
+  ch: ConfirmChannel,
+  exchange: string,
+  routingKey: string,
+  value: T,
+): Promise<void> {
+  const encodedValue = encode(value);
+  const buffer = Buffer.from(encodedValue.buffer, encodedValue.byteOffset, encodedValue.byteLength);
+  ch.publish(exchange, routingKey, buffer, { contentType: "application/x-msgpack" });
 }
